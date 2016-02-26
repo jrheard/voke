2a654da29803e373e728d9615fcb3e56f1870e61
jrheard <jrheard@cs.stanford.edu>
2016-02-26 15:32:24 -0800

    notes

--- src/voke/system/rendering.cljs
+++ src/voke/system/rendering.cljs
@@ -3,18 +3,18 @@
             [voke.schemas :refer [Entity System]])
   (:require-macros [schema.core :as sm]))
 
-(sm/defn render-entity
-  [ctx
-   entity :- Entity]
-  ; TODO  get a good canvas library via cljsjs, fabric or phaser seem to be top contenders, research
-  (aset ctx "fillStyle" "rgb(50,50,50)")
-  (.fillRect ctx
-             (-> entity :position :x)
-             (-> entity :position :y)
-             (-> entity :collision-box :width)
-             (-> entity :collision-box :height)))
+; ok. here is how things have to work
+; there are a few situations we need to account for
+; 1) level start - lots of entities exist, none (or maybe some!) have moved
+; 2) preexisting entity moves
+; 3) new entity is created
+; 4) old entity is removed
+; so we need to listen to :entity-created, :entity-removed, and also :movement.
+; and we need to be able to detect situations where there's an entity that we don't yet render
 
 (sm/defn render-system-tick [renderer stage entities publish-chan]
+  ;(let [unknown-entities ]) ; TODO add some sort of map of entitiy id to sprite or whatever
+  ; and detect if there are any entities in `entities` that aren't in our map atom
   (.render renderer stage)
   entities)
 
@@ -22,16 +22,19 @@
   ; TODO event has an :entity
   (js/console.log (clj->js event)))
 
-; TODO does this defonce fuck us figwheel-wise? irritating if so
-; gonna need to rethink this defonce.
-; gonna need to put some thought into how to make this whole system reloadable in general
-(defonce render-system
-         (let [renderer (js/PIXI.autoDetectRenderer. 800 600)
-               stage (js/PIXI.Container.)]
-           (.appendChild js/document.body (.-view renderer))
+(defonce ^:private rendering-engine
+         {:renderer (js/PIXI.autoDetectRenderer. 800 600)
+          :stage    (js/PIXI.Container.)
+          ; TODO likely also an atom mapping entity-id to sprite-or-equivalent js object
+          })
 
-           {:every-tick {:reads #{:position :render-info}
-                         :fn    (fn [& args]
-                                  (apply render-system-tick renderer stage args))}
-            :event-handlers [{:event-type :movement
-                              :fn a-rendering-event-handler}]}))
+(def render-system
+  (let [{:keys [renderer stage]} rendering-engine]
+
+    (.appendChild js/document.body (.-view renderer))
+
+    {:every-tick     {:reads #{:position :render-info}
+                      :fn    (fn [& args]
+                               (apply render-system-tick renderer stage args))}
+     :event-handlers [{:event-type :movement
+                       :fn         a-rendering-event-handler}]}))
