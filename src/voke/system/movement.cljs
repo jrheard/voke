cb6708ea9e34f1ffc4db74ec3e5ee627521da793
jrheard <jrheard@cs.stanford.edu>
2016-03-02 16:11:51 -0800

    don't continually apply friction over and over again so velocity approaches 0 infinitely, just stop when it gets low enough

--- src/voke/system/movement.cljs
+++ src/voke/system/movement.cljs
@@ -3,12 +3,13 @@
             [voke.schemas :refer [Entity GameState System]])
   (:require-macros [schema.core :as sm]))
 
+; TODO - make this a static map, calculated at compile time, that maps all the permutations of
+; up/down/left/right to their angle values; no need to do all this sin/cos/atan2 math on every tick
 (def direction-value-mappings {:down  (/ Math/PI 2)
                                :up    (- (/ Math/PI 2))
                                :left  Math/PI
                                :right 0})
 
-; TODO everything below here is super fucking rough
 (sm/defn update-orientation :- Entity
   [entity :- Entity]
   ; TODO - currently only implemented for player-controlled entities, doesn't handle monsters/projectiles
@@ -23,19 +24,26 @@
   [entity :- Entity]
   (let [acceleration (if (not-empty (entity :intended-move-direction))
                        (get-in entity [:motion :max-acceleration])
-                       0)
-        orientation (get-in entity [:shape :orientation])
-        update-axis-velocity (fn [trig-fn axis-value]
-                               (min (get-in entity [:motion :max-speed])
-                                    (* (+ axis-value
-                                          (* acceleration
-                                             (trig-fn orientation)))
-                                       0.9)))]
-    (-> entity
-        (update-in [:motion :velocity :x]
-                   (partial update-axis-velocity Math/cos))
-        (update-in [:motion :velocity :y]
-                   (partial update-axis-velocity Math/sin)))))
+                       0)]
+    (if (or (> acceleration 0)
+            (> (or (get-in entity [:motion :velocity :x] 0)
+                   (get-in entity [:motion :velocity :y] 0))))
+      (let [orientation (get-in entity [:shape :orientation])
+            update-axis-velocity (fn [trig-fn axis-value]
+                                   (let [new-value (min (get-in entity [:motion :max-speed])
+                                                        (* (+ axis-value
+                                                              (* acceleration
+                                                                 (trig-fn orientation)))
+                                                           0.9))]
+                                     (if (> (Math/abs new-value) 0.05)
+                                       new-value
+                                       0)))]
+        (-> entity
+            (update-in [:motion :velocity :x]
+                       (partial update-axis-velocity Math/cos))
+            (update-in [:motion :velocity :y]
+                       (partial update-axis-velocity Math/sin))))
+      entity)))
 
 (sm/defn update-position :- Entity
   [entity :- Entity]
@@ -61,6 +69,7 @@
                         ; if you're contacting a wall on the right, and you're trying to move up+right,
                         ; you *don't* move up because the position would be invalid!
                         ; you should be able to move up, though!
+
                         (publish-event publish-chan {:event-type   :intended-movement
                                                      :moved-entity (-> entity
                                                                        update-orientation
