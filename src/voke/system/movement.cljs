fe04390a3b9c7fd52f102c1f42199beeed6fe173
jrheard <jrheard@cs.stanford.edu>
2016-03-07 16:52:04 -0800

    super fucking hacky code, but now projectiles move

--- src/voke/system/movement.cljs
+++ src/voke/system/movement.cljs
@@ -38,64 +38,83 @@
   [entity :- Entity]
   (remove-conflicting-directions (safe-get-in entity [:brain :intended-move-direction])))
 
+; XXXX TODOOOO
+(defn should-update-velocity? [entity]
+  (and
+    (get-in entity [:brain :intended-move-direction])
+    (not-empty (human-controlled-entity-movement-directions entity)))
+  )
+
 (sm/defn update-orientation :- Entity
   [entity :- Entity]
   ; TODO - currently only implemented for player-controlled entities, doesn't handle monsters/projectiles
-  (assoc-in entity
-            [:shape :orientation]
-            (let [directions (human-controlled-entity-movement-directions entity)
-                  intended-direction-values (map direction-value-mappings directions)]
-              (Math/atan2 (apply + (map Math/sin intended-direction-values))
-                          (apply + (map Math/cos intended-direction-values))))))
+  ; TODO don't do any of this for projectiles
+  (if (should-update-velocity? entity)
+    (assoc-in entity
+              [:shape :orientation]
+              (let [directions (human-controlled-entity-movement-directions entity)
+                    intended-direction-values (map direction-value-mappings directions)]
+                (Math/atan2 (apply + (map Math/sin intended-direction-values))
+                            (apply + (map Math/cos intended-direction-values)))))
+    entity))
 
 (sm/defn update-velocity :- Entity
   [entity :- Entity]
   ; TODO - acceleration will be computed differently for AI-controlled monsters
-  (let [acceleration (if (not-empty (human-controlled-entity-movement-directions entity))
-                       (safe-get-in entity [:motion :max-acceleration])
-                       0)]
-    (if (or (> acceleration 0)
-            (> (or (safe-get-in entity [:motion :velocity :x] 0)
-                   (safe-get-in entity [:motion :velocity :y] 0))))
-      (let [orientation (safe-get-in entity [:shape :orientation])
-            update-axis-velocity (fn [trig-fn axis-velocity]
-                                   (let [new-velocity (min (safe-get-in entity [:motion :max-speed])
-                                                           (+ axis-velocity
-                                                              (* acceleration
-                                                                 (trig-fn orientation))))]
-                                     (if (> (Math/abs new-velocity) min-velocity)
-                                       new-velocity
-                                       0)))]
-        (-> entity
-            (update-in [:motion :velocity :x]
-                       (partial update-axis-velocity Math/cos))
-            (update-in [:motion :velocity :y]
-                       (partial update-axis-velocity Math/sin))))
-      entity)))
+  ; TODO don't do any of this for projectiles
+  (if-let [acceleration (when (should-update-velocity? entity)
+                          (safe-get-in entity [:motion :max-acceleration]))]
+    (let [orientation (safe-get-in entity [:shape :orientation])
+          update-axis-velocity (fn [trig-fn axis-velocity]
+                                 (let [new-velocity (min (safe-get-in entity [:motion :max-speed])
+                                                         (+ axis-velocity
+                                                            (* acceleration
+                                                               (trig-fn orientation))))]
+                                   (if (> (Math/abs new-velocity) min-velocity)
+                                     new-velocity
+                                     0)))]
+      (-> entity
+          (update-in [:motion :velocity :x]
+                     (partial update-axis-velocity Math/cos))
+          (update-in [:motion :velocity :y]
+                     (partial update-axis-velocity Math/sin))))
+    entity))
 
 (sm/defn apply-friction :- Entity
   [entity :- Entity]
-  (reduce
-    (fn [entity axis]
-      (update-in entity [:motion :velocity axis] #(* % friction-value)))
+  (if (= (get-in entity [:collision :type])
+         :projectile)
     entity
-    [:x :y]))
+    (reduce
+      (fn [entity axis]
+        (update-in entity [:motion :velocity axis] #(* % friction-value)))
+      entity
+      [:x :y])))
 
 (sm/defn update-position :- Entity
   [entity :- Entity]
-  (-> entity
-      (update-in [:shape :x]
-                 (fn [x]
-                   (+ x (safe-get-in entity [:motion :velocity :x]))))
-      (update-in [:shape :y]
-                 (fn [y]
-                   (+ y (safe-get-in entity [:motion :velocity :y]))))))
+  (reduce
+    (fn [entity axis]
+      (update-in entity
+                 [:shape axis]
+                 #(+ % (safe-get-in entity [:motion :velocity axis]))))
+    entity
+    [:x :y]))
+
+; XXXX TODO
+(defn relevant?
+  [entity]
+  (js/console.log (clj->js entity))
+  (or
+    (seq (get-in entity [:brain :intended-move-direction] nil))
+    (not= (get-in entity [:motion :velocity :x] 0) 0)
+    (not= (get-in entity [:motion :velocity :y] 0) 0)))
 
 ;; System definition
 
 (sm/def move-system :- System
   {:every-tick {:fn (fn move-system-tick [entities publish-chan]
-                      (doseq [entity (filter #(get-in % [:brain :intended-move-direction]) entities)]
+                      (doseq [entity (filter relevant? entities)]
                         (let [moved-entity (-> entity
                                                update-orientation
                                                update-velocity
