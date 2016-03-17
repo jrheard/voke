481fdcaa18574dfdca66d4ff81e48da147b6a7af
jrheard <jrheard@cs.stanford.edu>
2016-03-17 14:55:50 -0700

    notes

--- src/voke/util.cljs
+++ src/voke/util.cljs
@@ -15,3 +15,13 @@
     (< num lower) lower
     (> num upper) upper
     :else num))
+
+(defn winnow
+  [pred xs]
+  (let [matches (transient [])
+        not-matches (transient [])]
+    (doseq [x xs]
+      (if (pred x)
+        (conj! matches x)
+        (conj! not-matches x)))
+    [(persistent! matches) (persistent! not-matches)]))
