1415c59d68d74409652371d3186264a4ba7cb6d7
jrheard <jrheard@cs.stanford.edu>
2016-03-29 17:43:54 -0700

    haha holy shit it actually follows you around - seeing weird collision resolution issues though

--- src/voke/system/ai/system.cljs
+++ src/voke/system/ai/system.cljs
@@ -7,9 +7,17 @@
               (let [player (->> entities
                                 (filter #(contains? % :input))
                                 first)
+                    player-center (get-in player [:shape :center])
                     entities-with-ai (filter #(contains? % :ai) entities)]
                 (for [entity entities-with-ai]
-                  entity
+                  (let [entity-center (get-in entity [:shape :center])]
+                    (assoc-in entity
+                              [:motion :direction]
+                              (Math/atan2 (- (player-center :y)
+                                             (entity-center :y))
+                                          (- (player-center :x)
+                                             (entity-center :x)))))
+
                   )
                 )
               )})
