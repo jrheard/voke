4dc90b5c8298e80f45e3fd4357536557e9fd1303
jrheard <jrheard@cs.stanford.edu>
2016-09-24 14:13:19 -0700

    finish testing voke.state

--- test/voke/state_test.cljs
+++ test/voke/state_test.cljs
@@ -3,6 +3,9 @@
             [voke.state :as state]))
 
 (def blank-game-state (state/make-game-state []))
+(def game-state-with-an-entity (assoc-in blank-game-state
+                                         [:entities 0]
+                                         {:id 0 :renderable false}))
 
 (deftest adding-entities
   (with-redefs [state/buffer (atom [])]
@@ -13,3 +16,18 @@
              {:id 0 :renderable false})))))
 
 
+(deftest updating-entities
+  (with-redefs [state/buffer (atom [])]
+
+    (state/update-entity! 0 :combat-system (fn [entity] (assoc entity :renderable true)))
+
+    (let [updated-state (state/flush! game-state-with-an-entity)]
+      (is (= (get-in updated-state [:entities 0])
+             {:id 0 :renderable true})))))
+
+(deftest removing-entities
+  (with-redefs [state/buffer (atom [])]
+    (state/remove-entity! 0 :combat-system)
+    (let [updated-state (state/flush! game-state-with-an-entity)]
+      (is (= false
+             (contains? (updated-state :entities) 0))))))
