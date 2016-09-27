8cbbdb1f8af235cc0b0fad9044d29d94dca76a19
jrheard <jrheard@cs.stanford.edu>
2016-09-27 12:33:05 -0700

    clean up collision test a bit

--- test/voke/system/collision_test.cljs
+++ test/voke/system/collision_test.cljs
@@ -8,36 +8,37 @@
             [voke.system.collision.util :as collision-util]
             [voke.test-utils :refer [blank-game-state]]))
 
+(defn make-entity [id x y]
+  {:id        id
+   :collision {:type :foo}
+   :shape     {:width  10
+               :height 10
+               :center {:x x :y y}}})
+
+(defn prepare-for-collision-test [entities collision-events-atom]
+  (doseq [entity entities]
+    (collision-util/-track-entity entity))
+
+  (events/subscribe-to-event :contact
+                             (fn [event]
+                               (swap! collision-events-atom conj event)))
+
+  (assoc blank-game-state
+         :entities
+         (into {}
+               (map (juxt :id identity) entities))))
+
 (deftest two-entities-moving-into-each-other
-  (let [entity-1 {:id        1
-                  :collision {:type :foo}
-                  :shape     {:width  10
-                              :height 10
-                              :center {:x 100
-                                       :y 100}}}
-        entity-2 {:id        2
-                  :collision {:type :foo}
-                  :shape     {:width  10
-                              :height 10
-                              :center {:x 115
-                                       :y 115}}}
-        game-state (-> blank-game-state
-                       (assoc-in [:entities 1] entity-1)
-                       (assoc-in [:entities 2] entity-2))
+  (let [entity-1 (make-entity 1 100 100)
+        entity-2 (make-entity 2 115 115)
         collision-events (atom [])
-        apply-movement-calls (atom [])]
-
-    (collision-util/-track-entity entity-1)
-    (collision-util/-track-entity entity-2)
-
-    (events/subscribe-to-event :contact
-                               (fn [event]
-                                 (swap! collision-events conj event)))
-
+        apply-movement-calls (atom [])
+        game-state (prepare-for-collision-test [entity-1 entity-2] collision-events)]
 
     (with-redefs [state/contacts-fired (atom #{})
                   collision-util/apply-movement (fn [& args]
                                                   (swap! apply-movement-calls conj args))]
+      ; Two 10x10 entities, moving diagonally into the same spot.
       (system/attempt-to-move! entity-1
                                {:x 105 :y 105}
                                {:x 5 :y 5}
@@ -57,12 +58,8 @@
              [[entity-1 {:x 104.9 :y 104.9} {:x 0 :y 0}]
               [entity-2 {:x 99.9 :y 99.9} {:x 0 :y 0}]])))
 
-
     (js/Collision.resetState)))
 
 ; next up: entity is moving up+right, is next to a wall on its right
-
-
 ; also test dead entities / projectiles
-
 ; test that collision type / collides-with works
