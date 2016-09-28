06ceb830c48436a85996a99a017adf09bdc8d4d5
jrheard <jrheard@cs.stanford.edu>
2016-09-28 14:08:12 -0700

    switch to MIT license

--- project.clj
+++ project.clj
@@ -1,20 +1,24 @@
 (defproject voke "0.1.0-SNAPSHOT"
   :description "a roguelike"
-  :url "http://example.com/FIXME"
-  :license {:name "DO WHAT THE FUCK YOU WANT TO BUT IT'S NOT MY FAULT PUBLIC LICENSE"
-            :url  "https://raw.githubusercontent.com/adversary-org/wtfnmf/c7b46d8114e3b3adcd9198e635b43f511c7c803d/COPYING.WTFNMFPL"}
+  :url "http://jrheard.com/voke"
+  :license {:name "MIT License"
+            :url  "http://choosealicense.com/licenses/mit/"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.229"]
                  [org.clojure/core.async "0.2.391"
                   :exclusions [org.clojure/tools.reader]]
+
+                 ; dev
                  [binaryage/devtools "0.8.2"]
                  [figwheel-sidecar "0.5.7"]
+
                  [prismatic/plumbing "0.5.3"]
                  [prismatic/schema "1.1.3"]
                  [cljsjs/pixi "3.0.10-0"]]
 
   :plugins [[lein-cljsbuild "1.1.4" :exclusions [[org.clojure/clojure]]]
+            ; dev
             [lein-doo "0.1.7" :exclusions [[org.clojure/clojurescript]]]]
 
   :source-paths ["src"]
@@ -27,6 +31,13 @@
                 :figwheel     {:on-jsload "voke.core/main"}
                 :compiler     {:main                 voke.core
                                :preloads             [devtools.preload]
+                               :foreign-libs  [{:file     "resources/public/js/rbush.js"
+                                                :provides ["rbush"]
+                                                :module-type :commonjs}
+                                               {:file     "resources/public/js/collision.js"
+                                                :requires ["rbush"]
+                                                :provides ["collision"]
+                                                :module-type :commonjs}]
                                :asset-path           "js/compiled/out"
                                :output-to            "resources/public/js/compiled/voke.js"
                                :output-dir           "resources/public/js/compiled/out"
@@ -40,13 +51,18 @@
                                :output-dir    "resources/public/js/compiled/out-min"
                                :asset-path    "js/compiled/out-min"
                                :source-map    "resources/public/js/compiled/voke.js.map"
-                               :externs       ["externs/collision.js"]
+                               :foreign-libs  [{:file     "resources/public/js/rbush.js"
+                                                :provides ["rbush"]
+                                                :module-type :commonjs}
+                                               {:file     "resources/public/js/collision.js"
+                                                :requires ["rbush"]
+                                                :provides ["collision"]
+                                                :module-type :commonjs}]
                                :pretty-print  false}}
                {:id           "test"
                 :source-paths ["src" "test"]
                 :compiler     {:output-to     "test_resources/test.js"
                                :main          voke.runner
-                               :verbose true
                                :foreign-libs  [{:file     "resources/public/js/rbush.js"
                                                 :provides ["rbush"]
                                                 :module-type :commonjs}
