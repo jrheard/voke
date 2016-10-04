aaa21b2a8201391ec90b1d5d4b2ac4fa70e66ed4
jrheard <jrheard@cs.stanford.edu>
2016-10-04 11:46:03 -0700

    WIP on collision test macro

--- test/voke/system/collision_test.cljs
+++ test/voke/system/collision_test.cljs
@@ -3,12 +3,11 @@
             [js-collision]
             [rbush]
             [voke.events :as events]
+            [voke.system.collision-test-macros :refer-macros [with-collision-env]]
             [voke.system.collision.system :as system]
             [voke.system.collision.state :as state]
             [voke.system.collision.util :as collision-util]
             [voke.test-utils :refer [blank-game-state]]))
-
-; ok it's seriously time to make a macro
 
 (defn make-entity
   ([id x y] (make-entity id x y {:collision/type :gold}))
@@ -38,14 +37,8 @@
 
 (deftest two-entities-moving-into-each-other-diagonally
   (let [entity-1 (make-entity 1 100 100)
-        entity-2 (make-entity 2 115 115)
-        collision-events (atom [])
-        apply-movement-calls (atom [])
-        game-state (prepare-for-collision-test [entity-1 entity-2] collision-events)]
-
-    (with-redefs [state/contacts-fired (atom #{})
-                  collision-util/apply-movement (fn [& args]
-                                                  (swap! apply-movement-calls conj args))]
+        entity-2 (make-entity 2 115 115)]
+    (with-collision-env [entity-1 entity-2] collision-events apply-movement-calls game-state
       ; Two 10x10 entities, moving diagonally into the same spot.
       (system/attempt-to-move! entity-1
                                #:geometry{:x 105 :y 105}
@@ -54,19 +47,29 @@
       (system/attempt-to-move! entity-2
                                #:geometry{:x 105 :y 105}
                                #:geometry{:x 5 :y 5}
-                               (vals (game-state :game-state/entities))))
+                               (vals (game-state :game-state/entities)))
 
-    (testing "when two entities try to move into the same space, only one collision event should be fired"
-      (is (= @collision-events
-             [{:event/type :contact
-               :entities   [entity-1 entity-2]}])))
+      (testing "when two entities try to move into the same space, only one collision event should be fired"
+        (is (= @collision-events
+               [{:event/type :contact
+                 :entities   [entity-1 entity-2]}])))
 
-    (testing "diagonal collision resolution works correctly on corners"
-      (is (= @apply-movement-calls
-             [[entity-1 #:geometry{:x 104.9 :y 104.9} #:geometry{:x 0 :y 0}]
-              [entity-2 #:geometry{:x 99.9 :y 99.9} #:geometry{:x 0 :y 0}]]))))
+      (testing "diagonal collision resolution works correctly on corners"
+        (is (= @apply-movement-calls
+               [[entity-1 #:geometry{:x 104.9 :y 104.9} #:geometry{:x 0 :y 0}]
+                [entity-2 #:geometry{:x 99.9 :y 99.9} #:geometry{:x 0 :y 0}]]))))))
 
-  (js/Collision.resetState))
+
+#_(deftest two-entities-moving-into-each-other-diagonally
+    (let [entity-1 (make-entity 1 100 100)
+          entity-2 (make-entity 2 115 115)
+          collision-events (atom [])
+          apply-movement-calls (atom [])
+          game-state (prepare-for-collision-test [entity-1 entity-2] collision-events)]
+
+      )
+
+    (js/Collision.resetState))
 
 (deftest one-entity-moving-against-another
   (let [entity-1 (make-entity 1 100 100)
