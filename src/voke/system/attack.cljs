868abe41ed29f226e0c8287a2e8422b5916c3539
jrheard <jrheard@cs.stanford.edu>
2016-10-05 15:01:37 -0700

    tidy the attack system a bit

--- src/voke/system/attack.cljs
+++ src/voke/system/attack.cljs
@@ -7,7 +7,9 @@
 
 (def maximum-entity-velocity-shot-speed-contribution 2)
 
-;; everything in here is gross and that is ok for now
+(def things-all-projectiles-collide-with
+  #{:obstacle
+    :item})
 
 (defn can-attack?
   [entity]
@@ -36,7 +38,7 @@
                :axis :geometry/axis)
   :ret number?)
 
-(defn shot-speed
+(defn shot-speed-on-axis
   [entity axis]
   (let [weapon (entity :component/weapon)
         trig-fn (if (= axis :geometry/x) Math/cos Math/sin)]
@@ -44,37 +46,34 @@
           (weapon :weapon/shot-speed))
        (entity-velocity-contribution entity axis))))
 
-(s/fdef shot-speed
+(s/fdef shot-speed-on-axis
   :args (s/cat :entity :entity/entity
                :axis :geometry/axis))
 
-; TODO why does this function exist? what is its purpose? docstring it
-(defn -collides-with
+(defn -projectile-collides-with
+  "Returns the set of things that an entity's projectiles collide with."
   [entity]
-  (hash-set
-    :obstacle
-    :item
-    (if (= (get-in entity [:component/collision :collision/type]) :good-guy)
-      :bad-guy
-      :good-guy)))
+  (conj things-all-projectiles-collide-with
+        (if (= (get-in entity [:component/collision :collision/type]) :good-guy)
+          :bad-guy
+          :good-guy)))
 
-(s/fdef -collides-with
+(s/fdef -projectile-collides-with
   :args (s/cat :entity :entity/entity)
-  :ret (s/coll-of :collision/type :kind set? :into #{}))
+  :ret :collision/collides-with)
 
 ; TODO what does this function do? document it
 (defn -halves-along-dimension
   [entity axis]
   (let [side (if (= axis :geometry/x) :shape/width :shape/height)]
-    [(/ (get-in entity [:component/shape side]) 2)
-     (/ (get-in entity [:component/weapon :weapon/projectile-shape side]) 2)]))
+    (/ (+ (get-in entity [:component/shape side])
+          (get-in entity [:component/weapon :weapon/projectile-shape side]))
+       2)))
 
 (s/fdef -halves-along-dimension
   :args (s/cat :entity :entity/entity
                :axis :geometry/axis)
-  ; what the fuck are :entity-half and :projectile-half
-  :ret (s/cat :entity-half number?
-              :projectile-half number?))
+  :ret number?)
 
 (defn -pi-over
   [numerator denominator]
@@ -83,19 +82,19 @@
 (defn -projectile-center
   [entity]
   (let [direction (get-in entity [:component/weapon :weapon/fire-direction])
-        side (cond
-               (<= (-pi-over -3 4) direction (-pi-over -1 4)) :up
-               (<= (-pi-over -1 4) direction (-pi-over 1 4)) :right
-               (<= (-pi-over 1 4) direction (-pi-over 3 4)) :down
-               :else :left)
+        emission-direction (cond
+                             (<= (-pi-over -3 4) direction (-pi-over -1 4)) :up
+                             (<= (-pi-over -1 4) direction (-pi-over 1 4)) :right
+                             (<= (-pi-over 1 4) direction (-pi-over 3 4)) :down
+                             :else :left)
         center (get-in entity [:component/shape :shape/center])]
-    {:geometry/x (condp = side
-                   :left (apply - (center :geometry/x) (-halves-along-dimension entity :geometry/x))
-                   :right (apply + (center :geometry/x) (-halves-along-dimension entity :geometry/x))
+    {:geometry/x (condp = emission-direction
+                   :left (- (center :geometry/x) (-halves-along-dimension entity :geometry/x))
+                   :right (+ (center :geometry/x) (-halves-along-dimension entity :geometry/x))
                    (center :geometry/x))
-     :geometry/y (condp = side
-                   :up (apply - (center :geometry/y) (-halves-along-dimension entity :geometry/y))
-                   :down (apply + (center :geometry/y) (-halves-along-dimension entity :geometry/y))
+     :geometry/y (condp = emission-direction
+                   :up (- (center :geometry/y) (-halves-along-dimension entity :geometry/y))
+                   :down (+ (center :geometry/y) (-halves-along-dimension entity :geometry/y))
                    (center :geometry/y))}))
 
 (s/fdef -projectile-center
@@ -108,11 +107,11 @@
   [entity]
   (projectile (entity :entity/id)
               (-projectile-center entity)
-              (-collides-with entity)
+              (-projectile-collides-with entity)
               (get-in entity [:component/weapon :weapon/projectile-shape])
               (get-in entity [:component/weapon :weapon/projectile-color])
-              (shot-speed entity :geometry/x)
-              (shot-speed entity :geometry/y)))
+              (shot-speed-on-axis entity :geometry/x)
+              (shot-speed-on-axis entity :geometry/y)))
 
 (s/fdef make-projectile-for-entity
   :args (s/and (s/cat :entity :entity/entity)
@@ -126,6 +125,7 @@
   (flatten
     (for [entity (filter can-attack? entities)]
       (do
+        ; (Hopefully) the closest thing to a `(fire-missiles!)` call I'll ever write
         (add-entity! (make-projectile-for-entity entity) :attack-system)
         (assoc-in entity [:component/weapon :weapon/last-attack-timestamp] (now))))))
 
